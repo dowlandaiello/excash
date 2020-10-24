37ef9b69da03ffe086e08503a763386a875c85a8
Dowland Aiello <dowlandaiello@gmail.com>
2020-10-24 17:38:49 -0400

    excash: Config can now be parsed correctly.
    Signed-off-by: Dowland Aiello <dowlandaiello@gmail.com>

--- lib/excashd.ex
+++ lib/excashd.ex
@@ -1,15 +1,43 @@
 defmodule Excashd do
   def main(args) do
+    require Logger
+
     # Available excashd flags are:
     #
     # --datadir (-d): the root directory of the daemon - where persistent data
     #   will be kept and where the network.json file will be read from
-    options = [switches: [datadir: :string], aliases: [d: :datadir]]
-    defaults = [:datadir, File.cwd! <> "excash_data"]
+    options = [
+      switches: [datadir: :string, port: :integer, n_shards: :integer],
+      aliases: [d: :datadir, p: :port, n: :n_shards]
+    ]
 
-    {opts, _, _} = OptionParser.parse(args, options) |> hd |> Keyword.merge(defaults)
-    net_cfg = Net.Config.parse(options[:datadir] <> "network.json")
+    defaults = [{:datadir, File.cwd!() <> "/excash_data"}, {:port, 25565}, {:n_shards, 512}]
 
-    IO.inspect net_cfg
+    opts = elem(OptionParser.parse(args, options), 0) |> Keyword.merge(defaults)
+    cfg_file = opts[:datadir] <> "/network.json"
+
+    # Warn the user if the config is malformed
+    try do
+      case Net.Config.parse(cfg_file) do
+        {:error, _} ->
+          Logger.error(
+            "failed to open configuration file: make sure " <>
+              cfg_file <>
+              " exists and is readable by excash."
+          )
+
+        # See defaults being applied here in net/config.ex
+        cfg ->
+          safe_cfg = Map.merge(cfg, %Net.Config{})
+      end
+    rescue
+      Poison.DecodeError ->
+        Logger.error("failed to decode configuration: are you missing" <>
+      " `net_name` or `bootstrap_nodes`?")
+
+      Poison.ParseError ->
+        Logger.error("failed to parse configuration: make sure network." <>
+      "json contains valid JSON.")
+    end
   end
 end
---
+++ lib/net/supervisor.ex
@@ -0,0 +1,8 @@
+defmodule Net.Supervisor do
+  @moduledoc """
+  Implements a supervisor for the local node connected to an excash network -
+  the "hub" of excashd.
+  """
+
+  use Supervisor
+end
