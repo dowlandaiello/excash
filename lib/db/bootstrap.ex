a428c8063c493cdc2da2353e3251171e9d7635e9
Dowland Aiello <dowlandaiello@gmail.com>
2020-10-26 15:00:36 -0400

    db: Create bootstrap.ex.
    Signed-off-by: Dowland Aiello <dowlandaiello@gmail.com>

---
+++ lib/db/bootstrap.ex
@@ -0,0 +1,11 @@
+defmodule Db.Bootstrap do
+  @moduledoc """
+  Bootstraps the global state from the connected peers.
+  """
+
+  @doc """
+  Adopts / bootstraps the average state from the connected peers.
+  """
+  def synchronize_state() do
+  end
+end
